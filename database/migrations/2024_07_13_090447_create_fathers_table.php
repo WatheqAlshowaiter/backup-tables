c91d3a3616a72a1f9b14646d10a1d6411e3c9897
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-08-22 20:15:55 +0300

    refactor: improve postgres migration

--- database/migrations/2024_07_13_090447_create_fathers_table.php
+++ database/migrations/2024_07_13_090447_create_fathers_table.php
@@ -28,16 +28,20 @@ class CreateFathersTable extends Migration
                 $table->string('status')->storedAs("CASE WHEN active = 1 THEN 'Active' ELSE 'Inactive' END"); // (MySQL/PostgreSQL/SQLite)
             }
 
+            if ((float) App::version() >= Constants::VERSION_AFTER_STORED_AS_VIRTUAL_AS_SUPPORT && DB::getDriverName() == 'pgsql') {
+                $table->string('full_name')->storedAs("first_name || ' ' || last_name");
+                $table->string('status')->storedAs("CASE WHEN active THEN 'Active' ELSE 'Inactive' END");
+            }
+
             // todo SQL Server persisted()
 
             $table->timestamps(); // created_at, updated_at => ignored because they are nullable
         });
 
-        // todo postgres
-        if ((float) App::version() >= Constants::VERSION_AFTER_STORED_AS_VIRTUAL_AS_SUPPORT && DB::getDriverName() == 'pgsql') {
-            DB::statement(/**@lang PostgreSQL **/ "ALTER TABLE fathers ADD COLUMN full_name VARCHAR(255) GENERATED ALWAYS AS (first_name || ' ' || last_name) STORED");
-            DB::statement(/**@lang PostgreSQL **/ "ALTER TABLE fathers ADD COLUMN status VARCHAR(255) GENERATED ALWAYS AS (CASE WHEN active THEN 'Active' ELSE 'Inactive' END) STORED");
-        }
+        //if ((float) App::version() >= Constants::VERSION_AFTER_STORED_AS_VIRTUAL_AS_SUPPORT && DB::getDriverName() == 'pgsql') {
+        //    DB::statement(/**@lang PostgreSQL **/ "ALTER TABLE fathers ADD COLUMN full_name VARCHAR(255) GENERATED ALWAYS AS (first_name || ' ' || last_name) STORED");
+        //    DB::statement(/**@lang PostgreSQL **/ "ALTER TABLE fathers ADD COLUMN status VARCHAR(255) GENERATED ALWAYS AS (CASE WHEN active THEN 'Active' ELSE 'Inactive' END) STORED");
+        //}
 
     }
 
