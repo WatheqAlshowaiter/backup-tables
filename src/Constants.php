71ffc26f6fa2fef4cfdba0189ed4ced90c5fadf9
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-08-26 21:51:42 +0300

    refactor: remove unneeded constant

--- src/Constants.php
+++ src/Constants.php
@@ -6,8 +6,6 @@ class Constants
 {
     public const VERSION_AFTER_ULID_SUPPORT = 9.40;
 
-    public const VERSION_AFTER_FOREIGN_ID_SUPPORT = 7.0;
-
     public const VERSION_AFTER_UUID_SUPPORT = 7.0;
 
     public const VERSION_AFTER_STORED_AS_VIRTUAL_AS_SUPPORT = 8.0;
